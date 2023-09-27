4b64d4a7013804e6bff1a7ecbeedca41e6c1f9dc
Bryson lingle <brysonburkett99@gmail.com>
2023-09-27 00:06:27 -0600

    css

--- octaneclub/src/components/NewPost.vue
+++ octaneclub/src/components/NewPost.vue
@@ -1,14 +1,17 @@
 <template>
   <form @submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
     <label for="new-post-input">Add A New Post!</label>
+    </h2>
     <input
       type="text"
       id="new-post-input"
       name="new-post"
       autocomplete="off"
       v-model.lazy.trim="label"
-    />
-    <button type="submit">Add</button>
+    class="input_lg"/>
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
+
   
   </form>
 </template>
